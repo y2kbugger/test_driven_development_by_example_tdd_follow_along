7604be30aecec436c95e4688b97a4f604a050d85
y2k <git@y2kbugger.com>
2020-04-12 01:07:30 -0400

    Naive Franc

--- money.py
+++ money.py
@@ -9,4 +9,14 @@ class Dollar():
     def times(self, multiplier: int) -> 'Dollar':
         return Dollar(self._amount * multiplier)
 
+class Franc():
+    amount: int
+    def __init__(self, amount: int) -> None:
+        self._amount = amount
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Franc):
+            return NotImplemented
+        return self._amount == other._amount
+    def times(self, multiplier: int) -> 'Franc':
+        return Franc(self._amount * multiplier)
 
--- test_money.py
+++ test_money.py
@@ -1,4 +1,4 @@
-from money import Dollar
+from money import Dollar, Franc
 
 def test_that_tests_run():
     pass
@@ -9,8 +9,15 @@ def test_multiplication():
     five = Dollar(5)
 
     assert Dollar(10) == five.times(2)
-
     assert Dollar(15) == five.times(3)
+
+def test_franc_multiplication():
+    five: Franc
+
+    five = Franc(5)
+
+    assert Franc(10) == five.times(2)
+    assert Franc(15) == five.times(3)
 
 def test_equality():
     assert Dollar(5) == Dollar(5)
