d64223c02a9a2733096dd08fa3359e99f0d47782
y2k <git@y2kbugger.com>
2020-04-12 00:18:33 -0400

    Implement equals

--- money.py
+++ money.py
@@ -2,6 +2,11 @@ class Dollar():
     amount: int
     def __init__(self, amount: int) -> None:
         self.amount = amount
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Dollar):
+            return NotImplemented
+        return self.amount == other.amount
     def times(self, multiplier: int) -> 'Dollar':
         return Dollar(self.amount * multiplier)
+
 
--- test_money.py
+++ test_money.py
@@ -3,7 +3,7 @@ from money import Dollar
 def test_that_tests_run():
     pass
 
-def testMultiplication():
+def test_multiplication():
     five: Dollar
     product: Dollar
 
@@ -14,3 +14,7 @@ def testMultiplication():
 
     product = five.times(3)
     assert 15 == product.amount
+
+def test_equality():
+    assert Dollar(5) == Dollar(5)
+    assert Dollar(5) != Dollar(6)
