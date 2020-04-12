79c80de29c4acf4ebecacc70308e68f9f52249d7
y2k <git@y2kbugger.com>
2020-04-12 01:33:30 -0400

    Implement Chapter 7

--- money.py
+++ money.py
@@ -1,22 +1,19 @@
-class Dollar():
+class Money:
     amount: int
     def __init__(self, amount: int) -> None:
         self._amount = amount
     def __eq__(self, other: object) -> bool:
-        if not isinstance(other, Dollar):
+        if not isinstance(other, Money):
             return NotImplemented
+        if self.__class__ != other.__class__:
+            return False
         return self._amount == other._amount
+
+class Dollar(Money):
     def times(self, multiplier: int) -> 'Dollar':
         return Dollar(self._amount * multiplier)
 
-class Franc():
-    amount: int
-    def __init__(self, amount: int) -> None:
-        self._amount = amount
-    def __eq__(self, other: object) -> bool:
-        if not isinstance(other, Franc):
-            return NotImplemented
-        return self._amount == other._amount
+class Franc(Money):
     def times(self, multiplier: int) -> 'Franc':
         return Franc(self._amount * multiplier)
 
--- test_money.py
+++ test_money.py
@@ -22,3 +22,7 @@ def test_franc_multiplication():
 def test_equality():
     assert Dollar(5) == Dollar(5)
     assert Dollar(5) != Dollar(6)
+    assert Franc(5) == Franc(5)
+    assert Franc(5) != Franc(6)
+
+    assert Dollar(5) != Franc(5)
