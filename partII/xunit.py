0153f9573471e3670e68652bd4c1e099f7012925
y2k <git@y2kbugger.com>
2020-04-21 21:22:14 -0400

    Fix setup

--- partII/xunit.py
+++ partII/xunit.py
@@ -1,16 +1,16 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+        self.setup()
     def setup(self):
         self.was_setup  = True
     def run(self):
-        self.setup()
         method = self.__getattribute__(self.name)
         method()
 
 class WasRun(TestCase):
-    def __init__(self, name):
-        self.was_run = None
-        super().__init__(name)
+    def setup(self):
+        self.was_run = False
+        self.was_setup = True
     def test_method(self):
         self.was_run = True
