9db6340c2342bd81b9c6387b7d86c36200e66006
y2k <git@y2kbugger.com>
2020-04-21 20:52:38 -0400

    Finish first test and Chapter 18

--- partII/test_xunit.py
+++ partII/test_xunit.py
@@ -3,8 +3,8 @@ from xunit import WasRun, TestCase
 class TestCaseTest(TestCase):
     def test_running(self):
         test = WasRun("test_method")
-        print(test.was_run)
+        assert not test.was_run
         test.run()
-        print(test.was_run)
+        assert test.was_run
 
 TestCaseTest('test_running').run()
