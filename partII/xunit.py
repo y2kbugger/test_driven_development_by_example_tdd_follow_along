ae5faf3280f7b5587f3c7624dfd8b0212a2da71d
y2k <git@y2kbugger.com>
2020-04-21 21:27:18 -0400

    Move all "fake" flags to WasRun

--- partII/xunit.py
+++ partII/xunit.py
@@ -1,16 +1,22 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
-        self.setup()
     def setup(self):
-        self.was_setup  = True
+        pass
     def run(self):
+        self.setup()
         method = self.__getattribute__(self.name)
         method()
 
 class WasRun(TestCase):
+    def __init__(self, name):
+        self.was_run = False
+        self.was_setup = False
+        super().__init__(name)
+
     def setup(self):
         self.was_run = False
         self.was_setup = True
+
     def test_method(self):
         self.was_run = True
